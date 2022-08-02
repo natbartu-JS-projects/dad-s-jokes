79e94cc7dbd527c44d67b25ce757649dbcbdbe24
natacha <nat_bartu@hotmail.com>
2022-08-02 15:07:01 +1000

    fetch data from the api

---
+++ src/script.js
@@ -0,0 +1,18 @@
+const jokeElement = document.getElementById("joke");
+
+const jokeBtn = document.getElementById("jokeBtn");
+
+generateJoke();
+
+function generateJoke() {
+  const config = {
+    headers: {
+      Accept: "application/json",
+    },
+  };
+  fetch("https://icanhazdadjoke.com", config)
+    .then((response) => response.json())
+    .then((data) => {
+      jokeElement.innerHTML = data.joke;
+    });
+}
